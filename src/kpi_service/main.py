5e9677df6ab009dc3df512c399396f46c687506a
Martin Hartmann <martin1hartmann@web.de>
2025-06-19 07:24:17 +0200

    flake8 errors(2)

--- src/kpi_service/main.py
+++ src/kpi_service/main.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == "__main__":
     # ACHTUNG: absoluter Modulpfad, sonst findet Uvicorn dein App-Objekt nicht
     uvicorn.run(
-        "kpi_service.app:app",  #   ← genau so
+        "kpi_service.app:app",  # ← genau so
         host="127.0.0.1",
         port=8000,
         reload=True,  # Auto-Reload im Dev-Modus
